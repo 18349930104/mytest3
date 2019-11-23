3bcd51a8182e547d13f194f588ec92632062a63e
tangx <2550204323@qq.com>
2019-11-23 15:15:35 +0800

    修改了main方法

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,10 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("HelloWorld!");
-		System.out.println("HelloWorld!");
+		System.out.println("HelloShenzhen!");
+		System.out.println("Hello上海!");
+		System.out.println("Hello四川!");
+		System.out.println("Hello浙江!");
+		System.out.println("HelloChina!");
     }
 }
